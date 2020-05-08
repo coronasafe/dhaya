c46532624a54d8b5b876c6ab2b022885f112aa1f
aswinmohanme <dev.aswinmohan@gmail.com>
2020-05-08 11:34:28 +0530

    Add Appointment Scaffolding

--- src/views/PatientView.vue
+++ src/views/PatientView.vue
@@ -105,6 +105,119 @@
         <div class="md:col-span-1">
           <div class="px-4 sm:px-0">
             <h3 class="text-lg font-medium leading-6 text-gray-900">
+              Appointments
+            </h3>
+            <p class="mt-1 text-sm leading-5 text-gray-600">
+              Pending and Completed Appointments
+            </p>
+          </div>
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+          <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="bg-white shadow overflow-hidden sm:rounded-md">
+              <ul>
+                <li class="border-b border-gray-200">
+                  <a
+                    href="#"
+                    class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
+                  >
+                    <div class="flex items-center px-4 py-4 sm:px-6">
+                      <div class="min-w-0 flex-1 flex items-center">
+                        <div class="flex-shrink-0">
+                          <img
+                            class="h-12 w-12 rounded-full"
+                            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                          />
+                        </div>
+                        <div
+                          class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4"
+                        >
+                          <div>
+                            <div
+                              class="text-sm leading-5 font-medium text-indigo-600 truncate"
+                            >
+                              Ted Fox
+                            </div>
+                            <div
+                              class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                            >
+                              <svg
+                                class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                              >
+                                <path
+                                  fill-rule="evenodd"
+                                  d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884zM18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
+                                  clip-rule="evenodd"
+                                />
+                              </svg>
+                              <span class="truncate">ted.fox@example.com</span>
+                            </div>
+                          </div>
+                          <div class="hidden md:block">
+                            <div>
+                              <div class="text-sm leading-5 text-gray-900">
+                                Applied on
+                                <time datetime="2020-01-07"
+                                  >January 7, 2020</time
+                                >
+                              </div>
+                              <div
+                                class="mt-2 flex items-center text-sm leading-5 text-gray-500"
+                              >
+                                <svg
+                                  class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                                  fill="currentColor"
+                                  viewBox="0 0 20 20"
+                                >
+                                  <path
+                                    fill-rule="evenodd"
+                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                                    clip-rule="evenodd"
+                                  />
+                                </svg>
+                                Completed phone screening
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <svg
+                          class="h-5 w-5 text-gray-400"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="hidden sm:block">
+      <div class="py-5">
+        <div class="border-t border-gray-200"></div>
+      </div>
+    </div>
+
+    <div class="mt-10 sm:mt-0">
+      <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+          <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">
               Medical Information
             </h3>
             <p class="mt-1 text-sm leading-5 text-gray-600">
@@ -115,116 +228,7 @@
         <div class="mt-5 md:mt-0 md:col-span-2">
           <form action="#" method="POST">
             <div class="shadow overflow-hidden sm:rounded-md">
-              <div class="px-4 py-5 bg-white sm:p-6">
-                <div class="grid grid-cols-6 gap-6">
-                  <div class="col-span-6 sm:col-span-3">
-                    <label
-                      for="first_name"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >First name</label
-                    >
-                    <input
-                      id="first_name"
-                      class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    />
-                  </div>
-
-                  <div class="col-span-6 sm:col-span-3">
-                    <label
-                      for="last_name"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >Last name</label
-                    >
-                    <input
-                      id="last_name"
-                      class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    />
-                  </div>
-
-                  <div class="col-span-6 sm:col-span-4">
-                    <label
-                      for="email_address"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >Email address</label
-                    >
-                    <input
-                      id="email_address"
-                      class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    />
-                  </div>
-
-                  <div class="col-span-6 sm:col-span-3">
-                    <label
-                      for="country"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >Country / Region</label
-                    >
-                    <select
-                      id="country"
-                      class="mt-1 block form-select w-full py-2 px-3 py-0 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    >
-                      <option>United States</option>
-                      <option>Canada</option>
-                      <option>Mexico</option>
-                    </select>
-                  </div>
-
-                  <div class="col-span-6">
-                    <label
-                      for="street_address"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >Street address</label
-                    >
-                    <input
-                      id="street_address"
-                      class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    />
-                  </div>
-
-                  <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-                    <label
-                      for="city"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >City</label
-                    >
-                    <input
-                      id="city"
-                      class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    />
-                  </div>
-
-                  <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                    <label
-                      for="state"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >State / Province</label
-                    >
-                    <input
-                      id="state"
-                      class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    />
-                  </div>
-
-                  <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                    <label
-                      for="postal_code"
-                      class="block text-sm font-medium leading-5 text-gray-700"
-                      >ZIP / Postal</label
-                    >
-                    <input
-                      id="postal_code"
-                      class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                <button
-                  class="py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 shadow-sm hover:bg-indigo-500 focus:outline-none focus:shadow-outline-blue active:bg-indigo-600 transition duration-150 ease-in-out"
-                >
-                  Save
-                </button>
-              </div>
+              <div class="px-4 py-5 bg-white sm:p-6"></div>
             </div>
           </form>
         </div>
