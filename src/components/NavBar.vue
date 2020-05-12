6a5cf2a0de131f417bb98d2c97923ca191100707
aswinmohanme <dev.aswinmohan@gmail.com>
2020-05-12 12:44:50 +0530

    Add Login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -87,7 +87,7 @@
             </span>
             <span v-else class="rounded-md shadow-sm">
               <router-link
-                :to="{ name: 'PatientNew' }"
+                :to="{ name: 'UserLogin' }"
                 type="button"
                 class="relative inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-400 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-600 active:bg-indigo-600 transition duration-150 ease-in-out"
               >
